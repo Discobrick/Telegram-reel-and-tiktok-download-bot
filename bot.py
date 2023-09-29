45fca29eb7af0f0fe1c864de4f39044f89623f8a
Dioksy <dioksy@gmail.com>
2023-09-29 18:14:48 +0300

    Added donation link

--- bot.py
+++ bot.py
@@ -7,13 +7,16 @@ from telegram.ext import (ApplicationBuilder, ContextTypes, MessageHandler,
                           filters)
 
 from reelScrape import downloadReel
-
+donateAt = 0
 async def download(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     try:
         filePath = downloadReel(update.message.text,update.effective_user.username)
         await update.message.reply_video(filePath,caption='From @'+update.effective_user.username,quote=False,disable_notification=True,read_timeout=180,write_timeout=180,connect_timeout=180,pool_timeout=180)
         await update.effective_message.delete()
         os.remove(filePath)
+        donateAt += 1
+        if donateAt % 100 == 0:
+            await update.message.reply_text(text="Buy me a coffee: https://ko-fi.com/dioksy")
     except Exception:
         await update.message.reply_text(text="Can't download",quote=True)
         log.error('Error for link: ' + update.message.text)  
