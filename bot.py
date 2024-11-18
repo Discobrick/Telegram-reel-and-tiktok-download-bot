ecd2f47b1776509746313b1c86e8f098d693ad51
Dioksy <dioksy@gmail.com>
2024-11-18 18:51:10 +0200

    added truncation to messages

--- bot.py
+++ bot.py
@@ -7,6 +7,7 @@ from telegram.ext import (ApplicationBuilder, MessageHandler,filters)
 
 
 from scraper import download_reel
+import re
 
 global meme_bot_init,meme_bot_topic
 
@@ -17,7 +18,8 @@ async def download(update: Update,context) -> None:
     """Gets video url from a telegram message, downloads the video, and sends a message with it."""
     try:
         await update.message.set_reaction(reaction=ReactionTypeEmoji("👀"))
-        filePath = download_reel(update.message.text,update.effective_user.username)    
+        url = re.search(r'(https?://\S+)', update.message.text).group(0)
+        filePath = download_reel(url,update.effective_user.username)    
         if meme_bot_init == True and update.effective_chat.is_forum:
             await update.message.set_reaction(reaction=ReactionTypeEmoji("⚡"))
             await update.effective_chat.send_video(filePath,caption=from_user(update),disable_notification=True,read_timeout=600,write_timeout=600,connect_timeout=600,pool_timeout=600,message_thread_id=meme_bot_topic)
