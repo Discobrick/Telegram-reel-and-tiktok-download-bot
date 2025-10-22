113e6b9e2efe5d886eb70692c8c52123dd713e38
Emils Kaldbergs <dioksy@gmail.com>
2025-10-22 15:47:06 +0300

    Update user agent string in scraper.py for improved compatibility

--- scraper.py
+++ scraper.py
@@ -33,7 +33,7 @@ def download_reel_dlp(download_url, get_description=False):
         'format': 'bestvideo[vcodec^=avc]+bestaudio[ext=m4a]/best[ext=mp4]/best',
         'format_sort': ['res', 'ext:mp4:m4a', 'vcodec:h264'],
         'postprocessors': [{'key': 'FFmpegVideoConvertor', 'preferedformat': 'mp4'}],
-        'user_agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36'
+        'user_agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:144.0) Gecko/20100101 Firefox/144.0'
     }
 
     video_info = download_with_dlp(download_url, ydl_opts, get_description)
